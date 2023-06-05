9161fec23d22756a19c9c0e3c24f8c22104465e3
Ivan Putra Pratama <88358069+frctlprdx@users.noreply.github.com>
2023-06-05 15:51:39 +0700

    tugas9.cpp

--- Tugas kesembilan/tugas9.cpp
+++ Tugas kesembilan/tugas9.cpp
@@ -1,51 +1,78 @@
-// Insertion sort in C++
-
+// Online C++ compiler to run C++ program online
 #include <iostream>
-using namespace std;
-
-
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; i++) {
-    cout << array[i] << " ";
-  }
-  cout << endl;
+int jumlah(int a, int b){
+    if (b == 0) return a;
+    else return 1 + jumlah(a, b-1);
 }
 
-void insertionSort(int array[], int size) {
-  for (int step = 1; step < size; step++) {
-    int key = array[step];
-    int j = step - 1;
-    while (key > array[j] && j >= 0) {
-      array[j + 1] = array[j];
-      --j;
-    }
-    array[j + 1] = key;
-  }
+int kurang (int a, int b){
+    if (b == 0) return a;
+    else return kurang(a-1,b-1);
 }
 
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    for (i = 0; i < n - 1; i++)
-
-        for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
+int kali(int a, int b){
+    if(b==0) return 0;
+    else return a + kali(a,b-1);
 }
 
+int bagi(int a,int b){
+    if(a==0) return 0;
+    else return 1+bagi(a-b,b);
+}
 
+int pang(int a,int b){
+    if(b==0) return 1 ;
+    else return a * pang(a,b-1);
+}
 int main() {
-  int data[] = {9, 5, 1, 4, 3};
-  int x;
-  int size = sizeof(data) / sizeof(data[0]);
-  cout << "ketik 1 untuk insertion 2 untuk bubble sort";
-  cin >> x;
-
-  if (x=1){
-    insertionSort(data, size);}
-  else {
-    bubbleSort(data, size);
-  }
-  cout << "descending order:\n";
-  printArray(data, size);
+    int x,y,n;
+    std::cout << "Pilih Daftar\n";
+    std::cout << "1. Penjumlahan \n";
+    std::cout << "2. Pengurangan \n";
+    std::cout << "3. Perkalian\n";
+    std::cout << "4. Pembagian\n";
+    std::cout << "5. Pangkat\n";
+    std::cin>> n;
+    if (n == 1){
+    std::cout << "Masukkan X ";
+    std::cin>> x;
+    std::cout << "Masukkan Y ";
+    std::cin>> y;
+    int hasil = jumlah(x,y);
+    std::cout << hasil << std::endl;
+    }
+    else if (n==2){
+    std::cout << "Masukkan X ";
+    std::cin>> x;
+    std::cout << "Masukkan Y ";
+    std::cin>> y;
+    int hasil = kurang(x,y);
+    std::cout << hasil << std::endl;
+    }
+    else if (n==3){
+    std::cout << "Masukkan X ";
+    std::cin>> x;
+    std::cout << "Masukkan Y ";
+    std::cin>> y;  
+    int hasil = kali(x,y);
+    std::cout<<hasil<<std::endl;
+    }
+    else if (n==4){
+    std::cout << "Masukkan X ";
+    std::cin>> x;
+    std::cout << "Masukkan Y ";
+    std::cin>> y;  
+    int hasil = bagi(x,y);
+    std::cout<<hasil<<std::endl;
+    }
+    else if (n==5){
+    std::cout << "Masukkan X ";
+    std::cin>> x;
+    std::cout << "Masukkan Y ";
+    std::cin>> y;
+    int hasil = pang(x,y);
+    std::cout<<hasil<<std::endl;
+    }
+    
+    return 0;
 }
